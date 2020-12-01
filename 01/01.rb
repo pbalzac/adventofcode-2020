7e711aa36a1f091feb1b391f82bf77f3f85be760
pbalzac <pbalzac@tutanota.com>
2020-11-30 22:15:12 -0700

    updated

--- 01/01.rb
+++ 01/01.rb
@@ -1,21 +1,21 @@
-$nums = []
-
 def run(f)
+  nums = []
+
   File.readlines(f).each do |line|
     line.strip!
-    $nums << line.to_i
+    nums << line.to_i
   end
 
-  product = 0
-  (0...$nums.length).each do |i|
-    (i...$nums.length).each do |j|
-      (j...$nums.length).each do |k|
-        if $nums[i] + $nums[j] + $nums[k] == 2020
-          product = $nums[i] * $nums[j] * $nums[k]
-        end
+  product_of_two = 0
+  product_of_three = 0
+  (0...nums.length).each do |i|
+    (i...nums.length).each do |j|
+      product_of_two = nums[i] * nums[j] if nums[i] + nums[j] == 2020
+      (j...nums.length).each do |k|
+        product_of_three = nums[i] * nums[j] * nums[k] if nums[i] + nums[j] + nums[k]== 2020        
       end
     end
   end
 
-  product
+  [ product_of_two, product_of_three ]
 end
