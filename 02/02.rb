234893d1c3c825f33d831175b29f24c01e5ba2a4
pbalzac <pbalzac@tutanota.com>
2020-12-01 22:39:07 -0700

    2.1 & 2.2

---
+++ 02/02.rb
@@ -0,0 +1,17 @@
+def run(f)
+  valid_sled_passwords = 0
+  valid_toboggan_passwords = 0
+  File.readlines(f).each do |line|
+    line.strip!
+    m = line.match(/(\d+)-(\d+) (\w): (\w+)/)
+    a = m[1].to_i
+    b = m[2].to_i
+    letter = m[3]
+    password = m[4]
+    count = password.count(letter)
+    valid_sled_passwords += 1 if count >= a && count <= b
+    valid_toboggan_passwords += 1 if (password[a - 1] == letter) ? password[b - 1] != letter : password[b - 1] == letter
+  end
+
+  [ valid_sled_passwords, valid_toboggan_passwords ]
+end
