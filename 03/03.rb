2c97e206d1d6f83059e9917fd306d6995cba2073
pbalzac <pbalzac@tutanota.com>
2020-12-02 22:16:11 -0700

    3.1 & 3.2

---
+++ 03/03.rb
@@ -0,0 +1,21 @@
+def run(f)
+  map = []
+  File.readlines(f).each do |line|
+    line.strip!
+    map << line
+  end
+
+  trees_encountered = Array.new
+  map_width = map[0].length
+  [ [3, 1], [1, 1], [5, 1], [7, 1], [1, 2] ].each do | right, down |
+    trees = x = y = 0
+    until y >= map.length
+      trees += 1 if map[y][x] == '#'
+      x = (x + right) % map_width
+      y += down
+    end
+    trees_encountered << trees
+  end
+
+  [ trees_encountered[0], trees_encountered.reduce(:*) ]
+end
