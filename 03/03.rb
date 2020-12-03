b39ef073f59732068077a787ee6399acf434d1d7
pbalzac <pbalzac@tutanota.com>
2020-12-02 22:27:03 -0700

    step down

--- 03/03.rb
+++ 03/03.rb
@@ -4,11 +4,10 @@ def run(f)
   trees_encountered = Array.new
   map_width = map[0].length
   [ [3, 1], [1, 1], [5, 1], [7, 1], [1, 2] ].each do | right, down |
-    trees = x = y = 0
-    until y >= map.length
+    trees = x = 0
+    (0...map.length).step(down) do |y|
       trees += 1 if map[y][x] == '#'
       x = (x + right) % map_width
-      y += down
     end
     trees_encountered << trees
   end
