5d7848fa4df2ace66a3d8602ef856ad6ef94835a
pbalzac <pbalzac@tutanota.com>
2020-12-05 22:42:27 -0700

    combine iteration
    have to use a new set in the reduce so as not to alter original first
    set and thus affect the second calculation

--- 06/06.rb
+++ 06/06.rb
@@ -1,14 +1,15 @@
 require 'set'
 def run(f)
   groups = File.read(f).split("\n\n")
+
   any_answered_yes = 0
+  all_answered_yes = 0
+
   groups.each do |group|
-    any_answered_yes += group.split("\n").map{ |person| Set.new(person.chars) }.reduce(&:merge).size
+    answers = group.split("\n").map{ |person| Set.new(person.chars) }
+    any_answered_yes += answers.reduce(Set.new(answers[0]), &:merge).size
+    all_answered_yes += answers.reduce(Set.new(answers[0]), :&).size    
   end
 
-  all_answered_yes = 0
-  groups.each do |group|
-    all_answered_yes += group.split("\n").map{ |person| Set.new(person.chars) }.reduce(:&).size
-  end
   [ any_answered_yes, all_answered_yes ]
 end
