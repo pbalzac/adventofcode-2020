675231e6bbf7368856c0bb157b0e4a230b1b2512
pbalzac <pbalzac@tutanota.com>
2020-12-07 21:44:05 -0700

    extreme polish courtesy of mwood

--- 06/06.rb
+++ 06/06.rb
@@ -1,4 +1,3 @@
-require 'set'
 def run(f)
   groups = File.read(f).split("\n\n")
 
@@ -6,9 +5,9 @@ def run(f)
   all_answered_yes = 0
 
   groups.each do |group|
-    answers = group.split("\n").map{ |person| Set.new(person.chars) }
-    any_answered_yes += answers.reduce(Set.new(answers[0]), &:merge).size
-    all_answered_yes += answers.reduce(Set.new(answers[0]), :&).size    
+    answers = group.split("\n").map(&:chars)
+    any_answered_yes += answers.reduce(:|).size
+    all_answered_yes += answers.reduce(:&).size    
   end
 
   [ any_answered_yes, all_answered_yes ]
