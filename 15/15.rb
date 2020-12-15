40c432e2a4c3c27c72bd3b19cb79f272bd6dae80
pbalzac <pbalzac@tutanota.com>
2020-12-14 22:54:45 -0700

    15.1 & 15.2
    what i had before was a mess. still don't like the indexing or range here

--- 15/15.rb
+++ 15/15.rb
@@ -1,21 +1,16 @@
 def run(input, nth)
   spoken = Hash.new
   
-  input.each.with_index { |n, i|
-    spoken[n] = [ i + 1, -1 ]
+  input[0..-2].each.with_index { |n, i|
+    spoken[n] = i
   }
   last_spoken = input[-1]
   (input.length...nth).each { |i|
     this_spoken = 0
-    if !spoken[last_spoken].nil? && spoken[last_spoken][1] != -1
-      this_spoken = spoken[last_spoken][0] - spoken[last_spoken][1]
+    if !spoken[last_spoken].nil?
+      this_spoken = (i - 1) - spoken[last_spoken]
     end
-    if spoken[this_spoken].nil?
-      spoken[this_spoken] = [ i + 1, -1 ]
-    else
-      spoken[this_spoken][1] = spoken[this_spoken][0]
-      spoken[this_spoken][0] = i + 1
-    end
+    spoken[last_spoken] = i - 1
     last_spoken = this_spoken
   }
   last_spoken
