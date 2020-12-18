2c0a48d9bf81eb69b9d82ff3aa9cb19ff7adaee5
pbalzac <pbalzac@tutanota.com>
2020-12-17 23:40:42 -0700

    16.2
    nvm stayed with the stack contortions

--- 18/18.rb
+++ 18/18.rb
@@ -32,8 +32,7 @@ def run(f)
         op = stack[-1].pop
         stack[-1] << perform(op, $1.to_i, stack[-1].pop)
         $2.length.times {
-          x = stack.pop
-          num = x[0]
+          num = stack.pop.pop
           if stack[-1].empty?
             stack[-1] << num
           else
@@ -46,5 +45,51 @@ def run(f)
     answers << stack.pop.pop
   }
 
-  [ answers.reduce(:+) ]
+  answers_two = []
+  data.each { |row|
+    tokens = row.split(' ')
+    stack = [[]]
+    tokens.each { |token|
+      case token
+      when /^(\d+)$/
+        if stack[-1].empty? || stack[-1][-1] == :multiply
+          stack[-1] << $1.to_i
+        else
+          op = stack[-1].pop # will be addition
+          stack[-1] << perform(op, $1.to_i, stack[-1].pop)
+        end
+      when /^([+*])$/
+        stack[-1] << ($1 == '+' ? :add : :multiply)
+      when /^(\(+)(\d+)$/
+        $1.length.times { stack << [] }
+        stack[-1] << $2.to_i
+      when /^(\d+)(\)+)$/
+        stack[-1] << $1.to_i
+        $2.length.times {
+          until stack[-1].length == 1
+              b = stack[-1].pop
+              op = stack[-1].pop
+              a = stack[-1].pop
+              stack[-1] << perform(op, a, b)
+          end
+          num = stack.pop.pop
+          if stack[-1].empty? || stack[-1][-1] == :multiply
+            stack[-1] << num
+          else
+            op = stack[-1].pop
+            stack[-1] << perform(op, num, stack[-1].pop)
+          end
+        }
+      end
+    }
+    until stack[-1].length == 1
+      b = stack[-1].pop
+      op = stack[-1].pop
+      a = stack[-1].pop
+      stack[-1] << perform(op, a, b)
+    end
+    answers_two << stack.pop.pop
+  }
+  
+  [ answers.reduce(:+), answers_two.reduce(:+) ]
 end
