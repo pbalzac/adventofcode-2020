96c25e0e26311c1c733a6fda1f99eab8e9ae0e95
pbalzac <pbalzac@tutanota.com>
2020-12-30 12:17:27 -0700

    23.1 & 23.2
    rework

--- 23/23.rb
+++ 23/23.rb
@@ -1,132 +1,61 @@
-Cup = Struct.new(:num, :next, :prev, keyword_init: true)
+Cup = Struct.new(:num, :next, keyword_init: true)
 
-def printcups(current)
-  cup = current
-  c = Array.new
-  f = true
-  while f || cup != current
-    f = false
-    c << cup.num
-    cup = cup.next
+def run
+  input = '362981754'
+
+  c1 = run_cups(input)
+  c2 = run_cups(input, 10000000, 1000000)
+
+  s = c1[1].next
+  r = []
+  until s.num == 1
+    r << s.num
+    s = s.next
   end
-  p c
+
+  [ r.join.to_i, c2[1].next.num * c2[1].next.next.num ]
 end
 
-def run(n = 100)
-  input = '362981754'
-  #input = '389125467'
-  prev = nil
-  first = nil
-  nums = Array.new
+def run_cups(input, moves = 100, cups_up_to = 0)
+  first = prev = nil
+  cups = Hash.new
+
   input.chars.each { |c|
-    nums << c.to_i
     cup = Cup.new(num: c.to_i)
-    cup.prev = prev if !prev.nil?
+    cups[cup.num] = cup
     first = cup if first.nil?
+    prev.next = cup if !prev.nil?
     prev = cup
   }
-  first.prev = prev
-  prev.next = first
-  cup = prev
-  while cup != first
-    cup.prev.next = cup
-    cup = cup.prev
-  end
-    
 
-  mincup, maxcup = nums.minmax
-  
-  current = first
-  printcups(current)
-  n.times {
-    extract = current.next
-    extract_nums = [ current.next.num, current.next.next.num, current.next.next.next.num ]
-    current.next = current.next.next.next.next
-    current.next.prev = current
-    dnum = current.num - 1
-    if dnum < mincup then dnum = maxcup end
-    while extract_nums.include? dnum
-      dnum -= 1
-      if dnum < mincup then dnum = maxcup end
-    end
-    destination = current
-    while destination.num != dnum
-      destination = destination.next
-    end
-    dnext = destination.next
-    destination.next = extract
-    extract.prev = destination
-    extract.next.next.next = dnext
-    dnext.prev = extract.next.next
-
-    current = current.next
-    printcups(current)
-  }
-
-  
-  first
-end
-
-def run2(n = 10000000)
-  cuph = Hash.new
-  input = '362981754'
-  #input = '389125467'
-  prev = nil
-  first = nil
-  nums = Array.new
-  input.chars.each { |c|
-    nums << c.to_i
-    cup = Cup.new(num: c.to_i)
-    cuph[cup.num] = cup
-    cup.prev = prev if !prev.nil?
-    first = cup if first.nil?
-    prev = cup
-  }
-  (nums.max + 1..1000000).each { |i|
+  (cups.keys.max + 1..cups_up_to).each { |i|
     cup = Cup.new(num: i)
-    cuph[cup.num] = cup
-    cup.prev = prev
+    cups[cup.num] = cup
+    prev.next = cup
     prev = cup
   }
-  first.prev = prev
   prev.next = first
-  cup = prev
-  while cup != first
-    cup.prev.next = cup
-    cup = cup.prev
-  end
-    
-  mincup = nums.min
-  maxcup = 1000000
-  i = 0
+
+  mincup, maxcup = cups.keys.minmax
+
   current = first
-  n.times {
-    i += 1
-    if i % 100000 == 0
-      puts i
-    end
-    
+  moves.times {
     extract = current.next
-    extract_nums = [ current.next.num, current.next.next.num, current.next.next.next.num ]
+    extract_nums = [ extract.num, extract.next.num, extract.next.next.num ]
     current.next = current.next.next.next.next
-    current.next.prev = current
     dnum = current.num - 1
-    if dnum < mincup then dnum = maxcup end
+    dnum = maxcup if dnum < mincup
     while extract_nums.include? dnum
       dnum -= 1
-      if dnum < mincup then dnum = maxcup end
+      dnum = maxcup if dnum < mincup
     end
-    destination = cuph[dnum]
+    destination = cups[dnum]
     dnext = destination.next
     destination.next = extract
-    extract.prev = destination
     extract.next.next.next = dnext
-    dnext.prev = extract.next.next
 
     current = current.next
   }
 
-  one = cuph[1]
-
-  one.next.num * one.next.next.num
+  cups
 end
